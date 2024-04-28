017783752fd9cac3ba91109662537bbde5ded567
Matthew Callier <mcallier@gmail.com>
2024-04-27 20:25:53 -0700

    added x-xss-protection header

--- src/worker.js
+++ src/worker.js
@@ -31,7 +31,8 @@ export default {
         "Permissions-Policy": "document-domain=()",
         "Content-Security-Policy": "script-src 'self'; frame-ancestors 'none';",
         "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
-        "X-Robots-Tag": "noindex, noarchive, nosnippet"
+        "X-Robots-Tag": "noindex, noarchive, nosnippet",
+        "X-XSS-Protection": "1; mode=block"
       },
     });
   },
