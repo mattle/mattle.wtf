96951a4b06bc83c4054441bcd3e45f2f9c061942
Matthew Callier <mcallier@gmail.com>
2024-04-30 23:01:27 -0700

    party poopers

--- src/worker.js
+++ src/worker.js
@@ -25,14 +25,6 @@ body {
   padding: 0;
 }
 </style>
-<script>
-  window.onload = () => {
-    const glitch = document.getElementById('glitch');
-    setInterval(() => {
-      glitch.innerText = String.fromCharCode(Math.floor(Math.random() * 65535));
-    }, 100);
-  };
-</script>
 </head>
 <body>
 <marquee><h1>🦖🦖🦖 Hello! 🦖<span id="glitch">🦖</span>🦖</h1></marquee>
