1f1a9415f1f828880307ddf6c2b4e55056460cfb
Matthew Callier <mcallier@gmail.com>
2024-05-03 15:50:55 -0700

    refactor

--- src/worker.js
+++ src/worker.js
@@ -10,38 +10,38 @@
 
 export default {
   async fetch(request, env, ctx) {
-    return new Response(`<!DOCTYPE html>
-<html lang="en-US">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<title>Mattle, WTF?</title>
-<style>
-body {
-  font-family: Arial, sans-serif;
-  background-color: #0f0f0f;
-  color: #ccc;
-  margin: 0;
-  padding: 0;
-}
-</style>
-</head>
-<body>
-<marquee><h1>🦖🦖🦖 Hello! 🦖🦖🦖</h1></marquee>
-</body>
-</html>`, {
-      headers: {
-        "Content-Type":"text/html",
-        "Content-Encoding":"utf-8",
-        "X-Frame-Options": "DENY",
-        "X-Content-Type-Options": "nosniff",
-        "Referrer-Policy": "no-referrer",
-        "Permissions-Policy": "document-domain=()",
-        "Content-Security-Policy": "script-src 'self'; frame-ancestors 'none';",
-        "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
-        "X-Robots-Tag": "noindex, noarchive, nosnippet",
-        "X-XSS-Protection": "0"
-      },
-    });
+    const headers = {
+      "Content-Type":"text/html",
+      "Content-Encoding":"utf-8",
+      "X-Frame-Options": "DENY",
+      "X-Content-Type-Options": "nosniff",
+      "Referrer-Policy": "no-referrer",
+      "Permissions-Policy": "document-domain=()",
+      "Content-Security-Policy": "script-src 'self'; frame-ancestors 'none';",
+      "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+      "X-Robots-Tag": "noindex, noarchive, nosnippet",
+      "X-XSS-Protection": "0"
+    };
+    const html = `<!DOCTYPE html>
+      <html lang="en-US">
+      <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Mattle, WTF?</title>
+      <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #0f0f0f;
+        color: #ccc;
+        margin: 0;
+        padding: 0;
+      }
+      </style>
+      </head>
+      <body>
+      <marquee><h1>🦖🦖🦖 Hello! 🦖🦖🦖</h1></marquee>
+      </body>
+      </html>`;
+    return new Response(html, {headers: headers});
   },
 };
